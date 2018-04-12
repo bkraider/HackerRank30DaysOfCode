9bc4af9bbc8a688bc375bfad1905d16ae57d1823
bhanu korremula <bkraider@gmail.com>
2018-04-12 19:33:15 -0400

    Day 4 class vs instance variables

---
+++ HackerRank30DaysOfCode/Day4ClassvsInstance/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day4ClassvsInstance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int T = int.Parse(Console.In.ReadLine());
+            for (int i = 0; i < T; i++)
+            {
+                int age = int.Parse(Console.In.ReadLine());
+                Person p = new Person(age);
+                p.amIOld();
+                for (int j = 0; j < 3; j++)
+                {
+                    p.yearPasses();
+                }
+                p.amIOld();
+                Console.WriteLine();
+            }
+
+            Console.ReadLine();
+
+            /*
+             Sample Input
+             4
+            -1
+             10
+             16
+             18
+
+            Sample Output
+            Age is not valid, setting age to 0.
+            You are young.
+            You are young.
+
+            You are young.
+            You are a teenager.
+
+            You are a teenager.
+            You are old.
+
+            You are old.
+            You are old. 
+             */
+        }
+    }
+
+    class Person
+    {
+        public int age;
+        public Person(int initialAge)
+        {
+            if (initialAge < 0)
+            {
+                age = 0;
+                Console.WriteLine("Age is not valid, setting age to 0.");
+            }
+            else
+            {
+                age = initialAge;
+            }
+        }
+        public void amIOld()
+        {
+            if (age < 13)
+            {
+                Console.WriteLine("You are young.");
+            }
+            else if (age >= 13 && age < 18)
+            {
+                Console.WriteLine("You are a teenager.");
+            }
+            else
+            {
+                Console.WriteLine("You are old.");
+            }
+        }
+
+        public void yearPasses()
+        {
+            // Increment the age of the person in here
+            age = age + 1;
+        }
+    }
+}
