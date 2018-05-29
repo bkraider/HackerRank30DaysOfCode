f59425cc7de5262f6fe22ac2008ab6282c707e7b
bhanu korremula <bkraider@gmail.com>
2018-05-29 19:13:42 -0400

    recursion day 9

---
+++ HackerRank30DaysOfCode/Day9Recursion/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day9Recursion
+{
+    class Program
+    {
+        static int factorial(int n)
+        {
+
+            return n * n - 1;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int result = factorial(n);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
